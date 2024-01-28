7c25d8328c498bde6b63f6974b5583b12c7813ec
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2024-01-28 22:25:43 +0200

    commit

--- src/components/Vote/Vote.jsx
+++ src/components/Vote/Vote.jsx
@@ -1,71 +1,67 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Section from 'components/Section/Section';
 import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
 import Statistics from '../Statistics/Statistics';
 
-class Vote extends Component {
-  // static voteOptions = ['good', 'neutral', 'bad'];
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+const voteOptions = ['good', 'neutral', 'bad'];
 
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
+const Vote = () => {
+  const [votes, setVotes] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const countTotalFeedback = () => {
+    const { good, neutral, bad } = votes;
     const total = good + neutral + bad;
     return total;
-  }
+  };
 
-  countPositiveFeedback(keyName) {
-    const total = this.countTotalFeedback();
+  const countPositiveFeedback = keyName => {
+    const total = countTotalFeedback();
     if (!total) {
       return 0;
     }
-    const value = this.state[keyName];
+    const value = votes[keyName];
     return Number(((value / total) * 100).toFixed(2));
-  }
-
-  leaveFeedback = keyName => {
-    this.setState(prevState => {
-      return {
-        [keyName]: prevState[keyName] + 1,
-      };
-    });
   };
-  render() {
-    const total = this.countTotalFeedback();
-    const goodPercentage = this.countPositiveFeedback('good');
-    // const goodValue = this.state['good'];
-    // const neutralValue = this.state['neutral'];
-    // const badValue = this.state['bad'];
 
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <div>
-            <FeedbackOptions
-              options={Object.keys(this.state)}
-              leaveFeedback={this.leaveFeedback}
-            />
-          </div>
-        </Section>
-        <Section title="Statistics">
-          {total === 0 ? (
-            'No feedback given'
-          ) : (
-            <Statistics
-              goodValue={this.state.good}
-              neutralValue={this.state.neutral}
-              badValue={this.state.bad}
-              goodPercentage={goodPercentage}
-              total={total}
-            />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+  const leaveFeedback = keyName => {
+    setVotes(prevVotes => ({
+      ...prevVotes,
+      [keyName]: prevVotes[keyName] + 1,
+    }));
+  };
+
+  const total = countTotalFeedback();
+
+  const goodPercantage = countPositiveFeedback('good');
+  const goodValue = votes['good'];
+  const neutralValue = votes['neutral'];
+  const badValue = votes['bad'];
+
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <div>
+          <FeedbackOptions
+            options={voteOptions}
+            leaveFeedback={leaveFeedback}
+          />
+        </div>
+      </Section>
+      <Section title="Statistics">
+        {total === 0 ? (
+          'No feedback given'
+        ) : (
+          <Statistics
+            goodValue={goodValue}
+            neutralValue={neutralValue}
+            badValue={badValue}
+            goodPercantage={goodPercantage}
+            total={total}
+          />
+        )}
+      </Section>
+    </div>
+  );
+};
 
 export default Vote;
